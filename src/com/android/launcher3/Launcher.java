dccda0892299623e9d411fa00a8f7517d7205312
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-10-17 10:57:09 +0800

    [BUGFIX] fix hotseat disappearance issues
    Change-Id: I34459192e34a0431338a7d39610106465d0530e1
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1341,6 +1341,7 @@ public class Launcher extends StatefulActivity<LauncherState>
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection();
         BoostHelper.setThreadAffinity(Process.myPid(), 0);
+        LauncherStatesHelper.setActivityResumed(true);
     }
 
     @Override
@@ -1361,6 +1362,7 @@ public class Launcher extends StatefulActivity<LauncherState>
         }
         mAppWidgetHolder.setActivityResumed(false);
         BoostHelper.setThreadAffinity(Process.myPid(), 1);
+        LauncherStatesHelper.setActivityResumed(false);
     }
 
     /**
