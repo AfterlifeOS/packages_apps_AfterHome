crdroidandroid/android_packages_apps_Launcher3

Launcher3: Switch to AppLock API for protected and hidden apps
Squashed:

    From: Pranav Vashi <neobuddy89@gmail.com>
    Date: Sat, 4 Mar 2023 22:08:50 +0530
    Subject: Launcher3: Update AppLock API and integrate hidden apps

    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

Change-Id: I519dafb9b9e8df683df532dcaf35c29acb7306e5
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/lineage/trust/LoadTrustComponentsTask.java
@@ -15,25 +15,28 @@
  */
 package com.android.launcher3.lineage.trust;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.content.pm.PackageManager.PackageInfoFlags;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
 
+import com.android.internal.util.crdroid.Utils;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
-import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class LoadTrustComponentsTask extends AsyncTask<Void, Integer, List<TrustComponent>> {
     @NonNull
-    private TrustDatabaseHelper mDbHelper;
+    private AppLockHelper mAppLockHelper;
 
     @NonNull
     private PackageManager mPackageManager;
@@ -41,37 +44,46 @@ public class LoadTrustComponentsTask extends AsyncTask<Void, Integer, List<Trust
     @NonNull
     private Callback mCallback;
 
-    LoadTrustComponentsTask(@NonNull TrustDatabaseHelper dbHelper,
+    @NonNull
+    private Context mContext;
+
+    LoadTrustComponentsTask(@NonNull AppLockHelper appLockHelper,
             @NonNull PackageManager packageManager,
-            @NonNull Callback callback) {
-        mDbHelper = dbHelper;
+            @NonNull Callback callback,
+            @NonNull Context context) {
+        mAppLockHelper = appLockHelper;
         mPackageManager = packageManager;
         mCallback = callback;
+        mContext = context;
     }
 
     @Override
     protected List<TrustComponent> doInBackground(Void... voids) {
         List<TrustComponent> list = new ArrayList<>();
 
-        Intent filter = new Intent(Intent.ACTION_MAIN, null);
-        filter.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> apps = mPackageManager.queryIntentActivities(filter,
-                PackageManager.GET_META_DATA);
+        List<PackageInfo> apps = mPackageManager.getInstalledPackages(
+                    PackageInfoFlags.of(Long.valueOf(PackageManager.MATCH_ALL)));
 
         int numPackages = apps.size();
-        for (int i = 0; i < numPackages; i++) {
-            ResolveInfo app = apps.get(i);
-            try {
-                String pkgName = app.activityInfo.packageName;
-                String label = mPackageManager.getApplicationLabel(
-                        mPackageManager.getApplicationInfo(pkgName,
-                                PackageManager.GET_META_DATA)).toString();
-                Drawable icon = app.loadIcon(mPackageManager);
-                boolean isHidden = mDbHelper.isPackageHidden(pkgName);
-                boolean isProtected = mDbHelper.isPackageProtected(pkgName);
+        List<String> launchablePackages = Utils.launchablePackages(mContext);
+        List<String> whiteListedPackages = Arrays.asList(mContext.getResources().getStringArray(
+                com.android.internal.R.array.config_appLockAllowedSystemApps));
 
-                list.add(new TrustComponent(pkgName, icon, label, isHidden, isProtected));
+        for (int i = 0; i < numPackages; i++) {
+            PackageInfo app = apps.get(i);
+            try {
+                String pkgName = app.packageName;
+                if (!app.applicationInfo.isSystemApp() || launchablePackages.contains(pkgName) ||
+                        whiteListedPackages.contains(pkgName)) {
+                    String label = mPackageManager.getApplicationLabel(
+                            mPackageManager.getApplicationInfo(pkgName,
+                                    PackageManager.GET_META_DATA)).toString();
+                    Drawable icon = app.applicationInfo.loadIcon(mPackageManager);
+                    boolean isHidden = mAppLockHelper.isPackageHidden(pkgName);
+                    boolean isProtected = mAppLockHelper.isPackageProtected(pkgName);
+
+                    list.add(new TrustComponent(pkgName, icon, label, isHidden, isProtected));
+                }
 
                 publishProgress(Math.round(i * 100f / numPackages));
             } catch (PackageManager.NameNotFoundException ignored) {

src/com/android/launcher3/lineage/trust/TrustAppsAdapter.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2023 AlphaDroid
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +16,9 @@
  */
 package com.android.launcher3.lineage.trust;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Animatable2;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
@@ -29,6 +33,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.internal.util.crdroid.Utils;
 import com.android.launcher3.R;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 
@@ -39,8 +44,10 @@ class TrustAppsAdapter extends RecyclerView.Adapter<TrustAppsAdapter.ViewHolder>
     private List<TrustComponent> mList = new ArrayList<>();
     private Listener mListener;
     private boolean mHasSecureKeyguard;
+    private Context mContext;
 
-    TrustAppsAdapter(Listener listener, boolean hasSecureKeyguard) {
+    TrustAppsAdapter(Context context, Listener listener, boolean hasSecureKeyguard) {
+        mContext = context;
         mListener = listener;
         mHasSecureKeyguard = hasSecureKeyguard;
     }
@@ -95,10 +102,14 @@ class TrustAppsAdapter extends RecyclerView.Adapter<TrustAppsAdapter.ViewHolder>
 
             mHiddenView.setImageResource(component.isHidden() ?
                     R.drawable.ic_hidden_locked : R.drawable.ic_hidden_unlocked);
+
             mProtectedView.setImageResource(component.isProtected() ?
                     R.drawable.ic_protected_locked : R.drawable.ic_protected_unlocked);
 
             mProtectedView.setVisibility(hasSecureKeyguard ? View.VISIBLE : View.GONE);
+
+            mHiddenView.setVisibility(Utils.launchablePackages(mContext).contains(component.getPackageName()) ?
+                    View.VISIBLE : View.GONE);
 
             mHiddenView.setOnClickListener(v -> {
                 component.invertVisibility();

AfterlifeOS/packages_apps_AfterHome

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: HinohArata <mmgcntk@gmail.com>

src/com/android/launcher3/lineage/trust/LoadTrustComponentsTask.java
@@ -15,25 +15,28 @@
  */
 package com.android.launcher3.lineage.trust;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.content.pm.PackageManager.PackageInfoFlags;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
 
+import com.android.internal.util.voltage.VoltageUtils;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
-import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class LoadTrustComponentsTask extends AsyncTask<Void, Integer, List<TrustComponent>> {
     @NonNull
-    private TrustDatabaseHelper mDbHelper;
+    private AppLockHelper mAppLockHelper;
 
     @NonNull
     private PackageManager mPackageManager;
@@ -41,37 +44,46 @@ public class LoadTrustComponentsTask extends AsyncTask<Void, Integer, List<Trust
     @NonNull
     private Callback mCallback;
 
-    LoadTrustComponentsTask(@NonNull TrustDatabaseHelper dbHelper,
+    @NonNull
+    private Context mContext;
+
+    LoadTrustComponentsTask(@NonNull AppLockHelper appLockHelper,
             @NonNull PackageManager packageManager,
-            @NonNull Callback callback) {
-        mDbHelper = dbHelper;
+            @NonNull Callback callback,
+            @NonNull Context context) {
+        mAppLockHelper = appLockHelper;
         mPackageManager = packageManager;
         mCallback = callback;
+        mContext = context;
     }
 
     @Override
     protected List<TrustComponent> doInBackground(Void... voids) {
         List<TrustComponent> list = new ArrayList<>();
 
-        Intent filter = new Intent(Intent.ACTION_MAIN, null);
-        filter.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> apps = mPackageManager.queryIntentActivities(filter,
-                PackageManager.GET_META_DATA);
+        List<PackageInfo> apps = mPackageManager.getInstalledPackages(
+                    PackageInfoFlags.of(Long.valueOf(PackageManager.MATCH_ALL)));
 
         int numPackages = apps.size();
-        for (int i = 0; i < numPackages; i++) {
-            ResolveInfo app = apps.get(i);
-            try {
-                String pkgName = app.activityInfo.packageName;
-                String label = mPackageManager.getApplicationLabel(
-                        mPackageManager.getApplicationInfo(pkgName,
-                                PackageManager.GET_META_DATA)).toString();
-                Drawable icon = app.loadIcon(mPackageManager);
-                boolean isHidden = mDbHelper.isPackageHidden(pkgName);
-                boolean isProtected = mDbHelper.isPackageProtected(pkgName);
+        List<String> launchablePackages = VoltageUtils.launchablePackages(mContext);
+        List<String> whiteListedPackages = Arrays.asList(mContext.getResources().getStringArray(
+                com.android.internal.R.array.config_appLockAllowedSystemApps));
 
-                list.add(new TrustComponent(pkgName, icon, label, isHidden, isProtected));
+        for (int i = 0; i < numPackages; i++) {
+            PackageInfo app = apps.get(i);
+            try {
+                String pkgName = app.packageName;
+                if (!app.applicationInfo.isSystemApp() || launchablePackages.contains(pkgName) ||
+                        whiteListedPackages.contains(pkgName)) {
+                    String label = mPackageManager.getApplicationLabel(
+                            mPackageManager.getApplicationInfo(pkgName,
+                                    PackageManager.GET_META_DATA)).toString();
+                    Drawable icon = app.applicationInfo.loadIcon(mPackageManager);
+                    boolean isHidden = mAppLockHelper.isPackageHidden(pkgName);
+                    boolean isProtected = mAppLockHelper.isPackageProtected(pkgName);
+
+                    list.add(new TrustComponent(pkgName, icon, label, isHidden, isProtected));
+                }
 
                 publishProgress(Math.round(i * 100f / numPackages));
             } catch (PackageManager.NameNotFoundException ignored) {

src/com/android/launcher3/lineage/trust/TrustAppsAdapter.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2023 AlphaDroid
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +16,9 @@
  */
 package com.android.launcher3.lineage.trust;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Animatable2;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
@@ -29,6 +33,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.internal.util.voltage.VoltageUtils;
 import com.android.launcher3.R;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 
@@ -39,8 +44,10 @@ class TrustAppsAdapter extends RecyclerView.Adapter<TrustAppsAdapter.ViewHolder>
     private List<TrustComponent> mList = new ArrayList<>();
     private Listener mListener;
     private boolean mHasSecureKeyguard;
+    private Context mContext;
 
-    TrustAppsAdapter(Listener listener, boolean hasSecureKeyguard) {
+    TrustAppsAdapter(Context context, Listener listener, boolean hasSecureKeyguard) {
+        mContext = context;
         mListener = listener;
         mHasSecureKeyguard = hasSecureKeyguard;
     }
@@ -95,10 +102,14 @@ class TrustAppsAdapter extends RecyclerView.Adapter<TrustAppsAdapter.ViewHolder>
 
             mHiddenView.setImageResource(component.isHidden() ?
                     R.drawable.ic_hidden_locked : R.drawable.ic_hidden_unlocked);
+
             mProtectedView.setImageResource(component.isProtected() ?
                     R.drawable.ic_protected_locked : R.drawable.ic_protected_unlocked);
 
             mProtectedView.setVisibility(hasSecureKeyguard ? View.VISIBLE : View.GONE);
+
+            mHiddenView.setVisibility(VoltageUtils.launchablePackages(mContext).contains(component.getPackageName()) ?
+                    View.VISIBLE : View.GONE);
 
             mHiddenView.setOnClickListener(v -> {
                 component.invertVisibility();