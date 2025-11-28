crdroidandroid/android_packages_apps_Launcher3

Launcher3: Allow hiding navigation ime bar [4/4]
Change-Id: Ic764cab88b795f63cea85f142f1e39e6a2fadde3

quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
@@ -44,6 +44,7 @@ import static com.android.launcher3.taskbar.TaskbarAutohideSuspendController.FLA
 import static com.android.launcher3.taskbar.TaskbarAutohideSuspendController.FLAG_AUTOHIDE_SUSPEND_FULLSCREEN;
 import static com.android.launcher3.taskbar.TaskbarManager.ENABLE_TASKBAR;
 import static com.android.launcher3.taskbar.TaskbarManager.NAVIGATION_BAR_HINT;
+import static com.android.launcher3.taskbar.TaskbarManager.NAVBAR_IME_SPACE;
 import static com.android.launcher3.taskbar.TaskbarStashController.SHOULD_BUBBLES_FOLLOW_DEFAULT_VALUE;
 import static com.android.launcher3.testing.shared.ResourceUtils.getBoolByName;
 import static com.android.launcher3.util.Executors.UI_HELPER_EXECUTOR;
@@ -282,13 +283,13 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
                 mDeviceProfile.inv.numRows,
                 mDeviceProfile.inv.numColumns);
 
-        mImeDrawsImeNavBar = getBoolByName(IME_DRAWS_IME_NAV_BAR_RES_NAME, resources, false);
         mIsSafeModeEnabled = TraceHelper.allowIpcs("isSafeMode",
                 () -> getPackageManager().isSafeMode());
 
         // TODO(b/244231596) For shared Taskbar window, update this value in applyDeviceProfile()
         //  instead so to get correct value when recreating the taskbar
         SettingsCache settingsCache = SettingsCache.INSTANCE.get(this);
+        mImeDrawsImeNavBar = settingsCache.getValue(NAVBAR_IME_SPACE, 1);
         mIsUserSetupComplete = settingsCache.getValue(
                 Settings.Secure.getUriFor(Settings.Secure.USER_SETUP_COMPLETE), 0);
         mIsNavBarKidsMode = settingsCache.getValue(

AfterlifeOS/packages_apps_AfterHome

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: HinohArata <mmgcntk@gmail.com>

quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
@@ -44,6 +44,7 @@ import static com.android.launcher3.taskbar.TaskbarAutohideSuspendController.FLA
 import static com.android.launcher3.taskbar.TaskbarAutohideSuspendController.FLAG_AUTOHIDE_SUSPEND_FULLSCREEN;
 import static com.android.launcher3.taskbar.TaskbarManager.ENABLE_TASKBAR;
 import static com.android.launcher3.taskbar.TaskbarManager.NAVIGATION_BAR_HINT;
+import static com.android.launcher3.taskbar.TaskbarManager.NAV_BAR_IME;
 import static com.android.launcher3.taskbar.TaskbarStashController.SHOULD_BUBBLES_FOLLOW_DEFAULT_VALUE;
 import static com.android.launcher3.testing.shared.ResourceUtils.getBoolByName;
 import static com.android.launcher3.util.Executors.UI_HELPER_EXECUTOR;
@@ -282,13 +283,13 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
                 mDeviceProfile.inv.numRows,
                 mDeviceProfile.inv.numColumns);
 
-        mImeDrawsImeNavBar = getBoolByName(IME_DRAWS_IME_NAV_BAR_RES_NAME, resources, false);
         mIsSafeModeEnabled = TraceHelper.allowIpcs("isSafeMode",
                 () -> getPackageManager().isSafeMode());
 
         // TODO(b/244231596) For shared Taskbar window, update this value in applyDeviceProfile()
         //  instead so to get correct value when recreating the taskbar
         SettingsCache settingsCache = SettingsCache.INSTANCE.get(this);
+        mImeDrawsImeNavBar = settingsCache.getValue(NAV_BAR_IME, 1);
         mIsUserSetupComplete = settingsCache.getValue(
                 Settings.Secure.getUriFor(Settings.Secure.USER_SETUP_COMPLETE), 0);
         mIsNavBarKidsMode = settingsCache.getValue(