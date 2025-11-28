crdroidandroid/android_packages_apps_Launcher3

Launcher3: Allow changing the length of gesture navbar [2/3]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -124,6 +124,9 @@ public class TaskbarManager {
     public static final Uri NAVIGATION_BAR_HINT = LineageSettings.System.getUriFor(
             LineageSettings.System.NAVIGATION_BAR_HINT);
 
+    public static final Uri GESTURE_NAVBAR_LENGTH_MODE = Settings.System.getUriFor(
+            Settings.System.GESTURE_NAVBAR_LENGTH_MODE);
+
     private final Context mWindowContext;
     private final @Nullable Context mNavigationBarPanelContext;
     private WindowManager mWindowManager;
@@ -267,6 +270,8 @@ public class TaskbarManager {
                 .register(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .register(NAVIGATION_BAR_HINT, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mWindowContext)
+                .register(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
         Log.d(TASKBAR_NOT_DESTROYED_TAG, "registering component callbacks from constructor.");
         mWindowContext.registerComponentCallbacks(mDefaultComponentCallbacks);
         mShutdownReceiver.register(mWindowContext, Intent.ACTION_SHUTDOWN);
@@ -745,6 +750,8 @@ public class TaskbarManager {
                 .unregister(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .unregister(NAVIGATION_BAR_HINT, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mWindowContext)
+                .unregister(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
         Log.d(TASKBAR_NOT_DESTROYED_TAG, "unregistering component callbacks from destroy().");
         mWindowContext.unregisterComponentCallbacks(mDefaultComponentCallbacks);
         mShutdownReceiver.unregisterReceiverSafely(mWindowContext);

AfterlifeOS/packages_apps_AfterHome

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: HinohArata <mmgcntk@gmail.com>

quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -151,6 +151,9 @@ public class TaskbarManager implements DisplayDecorationListener {
     public static final Uri NAVIGATION_BAR_HINT = Settings.System.getUriFor(
             Settings.System.NAVIGATION_BAR_HINT);
 
+    public static final Uri GESTURE_NAVBAR_LENGTH_MODE = Settings.System.getUriFor(
+            Settings.System.GESTURE_NAVBAR_LENGTH_MODE);
+
     private final Context mBaseContext;
     private final int mPrimaryDisplayId;
     private final TaskbarNavButtonCallbacks mNavCallbacks;
@@ -477,6 +480,8 @@ public class TaskbarManager implements DisplayDecorationListener {
                 .register(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .register(NAVIGATION_BAR_HINT, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
+                .register(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
         SystemDecorationChangeObserver.getINSTANCE().get(mPrimaryWindowContext)
                 .registerDisplayDecorationListener(this);
         mShutdownReceiver =
@@ -1136,6 +1141,8 @@ public class TaskbarManager implements DisplayDecorationListener {
                 .unregister(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .unregister(NAVIGATION_BAR_HINT, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
+                .unregister(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
         SystemDecorationChangeObserver.getINSTANCE().get(mPrimaryWindowContext)
                 .unregisterDisplayDecorationListener(this);
         debugPrimaryTaskbar("destroy: unregistering component callbacks");