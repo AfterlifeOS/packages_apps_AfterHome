Musical143143/android_packages_apps_Launcher3

[BUGFIX] fix overview blur/depth focus issue
Change-Id: I57e31ca4fe75daf35318acf91c25d1d3737dcd4f
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

quickstep/src/com/android/quickstep/util/BaseDepthController.java
@@ -31,6 +31,7 @@ import com.android.launcher3.Flags;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
+import com.android.launcher3.util.LauncherStatesHelper;
 import com.android.launcher3.util.MultiPropertyFactory;
 import com.android.launcher3.util.MultiPropertyFactory.MultiProperty;
 
@@ -152,7 +153,10 @@ public class BaseDepthController {
     protected void onInvalidSurface() { }
 
     protected void applyDepthAndBlur() {
-        float depth = mDepth;
+        boolean forceDepthOrBlur = LauncherStatesHelper.isOverview();
+
+        float depth = forceDepthOrBlur ? 1 : mDepth;
+
         IBinder windowToken = mLauncher.getRootView().getWindowToken();
         if (windowToken != null) {
             if (enableScalingRevealHomeAnimation()) {
@@ -182,7 +186,7 @@ public class BaseDepthController {
         }
         mWaitingOnSurfaceValidity = false;
         boolean hasOpaqueBg = mLauncher.getScrimView().isFullyOpaque();
-        boolean isSurfaceOpaque = !mHasContentBehindLauncher && hasOpaqueBg && !mPauseBlurs;
+        boolean isSurfaceOpaque = !mHasContentBehindLauncher && hasOpaqueBg && !mPauseBlurs && !forceDepthOrBlur;
 
         float blurAmount;
         if (enableScalingRevealHomeAnimation()) {
@@ -190,8 +194,13 @@ public class BaseDepthController {
         } else {
             blurAmount = depth;
         }
-        mCurrentBlur = !mCrossWindowBlursEnabled || hasOpaqueBg || mPauseBlurs
+        
+        mCurrentBlur = !mCrossWindowBlursEnabled || hasOpaqueBg || mPauseBlurs 
                 ? 0 : (int) (blurAmount * mMaxBlurRadius);
+
+        if (forceDepthOrBlur && mCurrentBlur != mMaxBlurRadius) {
+            mCurrentBlur = mMaxBlurRadius;
+        }
 
         SurfaceControl.Transaction transaction = new SurfaceControl.Transaction();
         if (mBlurEnabled && mBlurSurface != null) {

AfterlifeOS/packages_apps_AfterHome

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: HinohArata <mmgcntk@gmail.com>

quickstep/src/com/android/quickstep/util/BaseDepthController.java
@@ -32,6 +32,7 @@ import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
+import com.android.launcher3.util.LauncherStatesHelper;
 import com.android.launcher3.util.MultiPropertyFactory;
 import com.android.launcher3.util.MultiPropertyFactory.MultiProperty;
 import com.android.systemui.shared.system.BlurUtils;
@@ -146,7 +147,10 @@ public class BaseDepthController {
     protected void onInvalidSurface() { }
 
     protected void applyDepthAndBlur() {
-        float depth = mDepth;
+        boolean forceDepthOrBlur = LauncherStatesHelper.isOverview();
+
+        float depth = forceDepthOrBlur ? 1 : mDepth;
+
         IBinder windowToken = mLauncher.getRootView().getWindowToken();
         if (windowToken != null) {
             if (enableScalingRevealHomeAnimation()) {
@@ -177,7 +181,7 @@ public class BaseDepthController {
         }
         mWaitingOnSurfaceValidity = false;
         boolean hasOpaqueBg = mLauncher.getScrimView().isFullyOpaque();
-        boolean isSurfaceOpaque = !mHasContentBehindLauncher && hasOpaqueBg && !mPauseBlurs;
+        boolean isSurfaceOpaque = !mHasContentBehindLauncher && hasOpaqueBg && !mPauseBlurs && !forceDepthOrBlur;
 
         float blurAmount;
         if (enableScalingRevealHomeAnimation()) {
@@ -185,8 +189,14 @@ public class BaseDepthController {
         } else {
             blurAmount = depth;
         }
-        mCurrentBlur = !mCrossWindowBlursEnabled || hasOpaqueBg || mPauseBlurs
+        
+        mCurrentBlur = !mCrossWindowBlursEnabled || hasOpaqueBg || mPauseBlurs 
                 ? 0 : (int) (blurAmount * mMaxBlurRadius);
+
+        int maxBlurInt = (int) mMaxBlurRadius;
+        if (forceDepthOrBlur && mCurrentBlur != maxBlurInt) {
+            mCurrentBlur = maxBlurInt;
+        }
 
         SurfaceControl.Transaction transaction = new SurfaceControl.Transaction();
         if (enableOverviewBackgroundWallpaperBlur() && mBlurSurface != null) {